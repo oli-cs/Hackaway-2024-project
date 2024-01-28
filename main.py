2958947d46dfee173e66570f45e30fa1ea229057
oli-cs <62400254+oli-cs@users.noreply.github.com>
2024-01-28 11:49:03 +0000

    Added more lore

--- main.py
+++ main.py
@@ -14,25 +14,31 @@ def on_change(state, var_name, var_value):
         match var_value :
             case  "Mailbox": 
                 if mailbox_locked == True:
-                    state.mansion_text = "You go to the mailbox. It is locked."
+                    state.mansion_text = "You go to the mailbox. It is locked, likely electronically."
+                    state.mansion_text2 = "A wire leads out the back of it and into the wall connecting to the garden."
                     return
                 
-                state.mansion_text = "You look in the mailbox. There is a single letter."
+                state.mansion_text = "You look inside the mailbox. There is a single letter."
+                state.mansion_text2 = ""
                 state.mailbox_pane = True
                 if not mailbox_created:
                     gui.add_page("Mail_Pane",mailbox_text,"main.css")
                 return
             case "Painting":
-                state.mansion_text = "You walk up to a painting. You see a note sticking out of its frame. It reads 1001."
+                state.mansion_text = "You walk up to a painting. It depicts a small family posing together; by the appearance of their clothes and the weathered look of the piece it must have been from a time long ago."
+                state.mansion_text2 = "You see a small white piece of paper sticking out the bottom of the frame. A note. It reads the number 1001."
                 return
             case "Party RSVP":
-                state.mansion_text = "You flip over an invite. Blank."
+                state.mansion_text = "You find a pile of ornate RSVP cards stacked neatly on a desk, their finishings glistening in the light."
+                state.mansion_text2 = "You pick one up and flip it over. Blank."
                 return
             case "Lampshade":
-                state.mansion_text = "You look under the lampshade. There is nothing but dust and cobwebs."
+                state.mansion_text = "You stumble across a mangled lamp in the dark. You attempt to turn it on, but nothing happens."
+                state.mansion_text2 = "You look under the lampshade. There is nothing but dust and cobwebs. A lone spider scurries across the desk."
                 return
             case "Rug":
-                state.mansion_text = "You look under the rug. The rug is blinking back at you."
+                state.mansion_text = "Whilst exploring the mansion's rooms, you feel the ground soften. You have stepped on a lavish rug, perhaps made from the skin of a hunted animal."
+                state.mansion_text2 = "You look under the rug, but find nothing. The rug is blinking back at you."
                 return
             
     # garden object selection
@@ -53,20 +59,24 @@ def on_change(state, var_name, var_value):
                 press("f5")
                 return
             case "Plant pots":
-                state.garden_text = "You look under the plant pots and there is nothing there. The name of the plant is italicised."
+                state.garden_text = "The walls of the garden are littered with plant pots of varying shape and size."
+                state.garden_text2 = "Glancing at the plant nearest you, it appears to be withered and dying, the name of its family italicised on a plaque."
                 return
             case "Pond":
-                state.garden_text = "You look into the pond. Fishes are staring back at you."
+                state.garden_text = "You look into the pond. Many fish swim around the water, bringing the odd splash of colour to its murky gray."
+                state.garden_text2 = "Peering further in, you can barely make out the bottom of the pond behind a cloud of silt and mud. Not somewhere worth searching further."
                 return
             case "Statue":
-                state.garden_text = "You notice a statue. Upon closer look, you see a control fixed to the back of it with a series of switched lining it."
-            
+                state.garden_text = "You notice a statue, overrun with vines and clearly not maintained."
+                state.garden_text2 = "Upon closer look, you see a control fixed to the back of it with a series of switched lining it."
+                
                 state.statue_pane = True
                 if not statue_created:
                     gui.add_page("Statue_Slider",statue_slider)
                 return
             case "Overgrown grass":
                 state.garden_text = "You step into the grass. It is too thick to go further."
+                state.garden_text2 = "The shrill cry of cicadas resonates out from the grass and through the cold air."
                 return
     
     # if passcode is correct, unlock door
@@ -77,7 +87,7 @@ def on_change(state, var_name, var_value):
                     "S",
                     "UNLOCKED"
                 ]
-                state.door_text = "You enter the code and hear the door click."
+                state.door_text = "You enter the code and hear the door click open."
                 state.door_dialog = True
                 gui.add_page("Blahaj",page4_md)
                 return
@@ -106,11 +116,12 @@ status = [
 ]
 
 # initialising values for user interaction and text
-rsvp = "words !!"
 greenhouse = []
 mansion_text = ""
+mansion_text2 = ""
 garden_text = ""
-door_text = ""
+garden_text2 = ""
+door_text = "The door is heavy, and will not budge. A heavy, gold padlock adornes the front."
 door_dialog = False
 mailbox_pane = False
 statue_pane = False
@@ -141,12 +152,10 @@ mailbox_text = """Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 
 statue_slider = "<|{toggle1}|toggle|lov=On;Off|unselected_value=Off|><|{toggle2}|toggle|lov=On;Off|unselected_value=Off|><|{toggle3}|toggle|lov=On;Off|unselected_value=Off|><|{toggle4}|toggle|lov=On;Off|unselected_value=Off|>"
 
-#statue_slider = "⠀⠀\n<|{slideVal}|slider|width=1600px|height=250px|id='statueSlider'|text_anchor=left|>"
-
 # page layouts - unchangable other than variables
-page1_md = "You find yourself locked in a mansion.\n\n <|{mansion_select}|selector|lov=Select an Item to Inspect;Mailbox;Painting;Party RSVP;Lampshade;Rug|dropdown|>\n<|{mailbox_pane}|pane|anchor=right|page=Mail_Pane|>\n<|{mansion_text}|text|>\n"
-page2_md = "You enter a garden.\n\n <|{garden_select}|selector|lov=Select an Item to Inspect;Greenhouse;Plant pots;Pond;Statue;Overgrown grass|dropdown|>\n\n<|{garden_text}|text|>\n\n\n<|{statue_pane}|pane|anchor=top|page=Statue_Slider|>\n<|{greenhouse}|table|>\n\n"
-page3_md = """  \n<|{passcode}|number|><|{status}|status|>\n<|{door_text}|text|>\n<|{door_dialog}|dialog|page=Blahaj|>
+page1_md = "You find yourself locked in a mansion.\n\n <|{mansion_select}|selector|lov=Select an Item to Inspect;Mailbox;Painting;Party RSVP;Lampshade;Rug|dropdown|>\n<|{mailbox_pane}|pane|anchor=right|page=Mail_Pane|>\n\n<|{mansion_text}|text|>\n\n<|{mansion_text2}|text|>\n"
+page2_md = "You enter a garden.\n\n <|{garden_select}|selector|lov=Select an Item to Inspect;Greenhouse;Plant pots;Pond;Statue;Overgrown grass|dropdown|>\n\n<|{garden_text}|text|>\n\n<|{garden_text2}|text|>\n\n<|{statue_pane}|pane|anchor=top|page=Statue_Slider|>\n<|{greenhouse}|table|>\n\n"
+page3_md = """  <|{door_text}|text|>\n\n<|{passcode}|number|><|{status}|status|>\n<|{door_dialog}|dialog|page=Blahaj|>\n
 
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⢖⠲⣶⡒⠒⣲⣶⡒⡲⠖⠚⡶⢦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡴⠲⣏⣅⠊⠀⠘⠃⠸⣿⣯⠳⠘⠂⠀⠘⣂⣿⣷⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n
@@ -177,7 +186,7 @@ page3_md = """  \n<|{passcode}|number|><|{status}|status|>\n<|{door_text}|text|>
 # blahaj hidden page
 page4_md = """ <|{status}|status|> \n
 
-This is BLAHAJ\n\n
+Pushing the door slowly open, you are greeted with a magnificent BLAHAJ waiting on the other side. Freedom. \n\n
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n
 ⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n
